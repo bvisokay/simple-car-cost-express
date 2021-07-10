6e76c7ce466f3d7f3507c7d21358d7acd572d200
brandon <bvisokay@gmail.com>
2021-07-10 12:45:53 -0400

    Tring to enforce ssl

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ const app = express()
 // Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
 // a load balancer (e.g. Heroku). See further comments below
 app.use(enforce.HTTPS({ trustProtoHeader: true }))
-http.createServer(app).listen(app.get("port"), function () {
+/* http.createServer(app).listen(app.get("port"), function () {
   console.log("Express server listening on port " + app.get("port"))
-})
+}) */
 
 // set up configuration object for sessions
 // not worth memorizing boilerplate
